a9b827b4623c11254d48e11ecc5ed6008310a711
abhishek agarwal <abhishekagarwal5662@gmail.com>
2021-11-10 13:48:29 +0530

    Update settings.py
    Secret key updated

--- noteapp/settings.py
+++ noteapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3lax)2x!-rn-u%a2#y601mwzm-of@jkflzdq)w&l*^hhdgx0f'
+SECRET_KEY = 'MY-SECRET-KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
